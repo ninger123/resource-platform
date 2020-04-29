5de32ec94fd6572e2711ad33b27ffdb92861d82c
ninger123 <1106697802@qq.com>
2020-04-29 14:34:05 +0800

    fix: 首页字体

--- src/views/applicant/home/index.vue
+++ src/views/applicant/home/index.vue
@@ -2,7 +2,6 @@
   <div class="home-container">
       <div class="content-container">
         <div class="img-container">
-          <img src="../pic/首页.jpg" />
           <div class="title">大数据与人工智能平台自助服务系统</div>
         </div>
       </div>
@@ -30,21 +29,24 @@ export default {
     position: relative;
 
     .content-container{
+
       .img-container{
-        position: relative;
-        img{
-          width: 100%;
-          height:800px;
-        }
+        width: 100%;
+        height:800px;
+        background-image: url(../pic/首页.jpg);
+        background-repeat:no-repeat;
+        background-size:100% 100%;
+        z-index:0;
+      }
         .title{
-          position: absolute;
-          left:400px;
-          top:365px;
+          width:1120px;
+          height: 800px;
+          margin: 0 auto;
           color: white;
           font-size: 70px;
+          padding:360px 0;
         }
       }
-    }
   }
 </style>
 
